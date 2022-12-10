59e4be3e061d4cd8428c6367c92e20f2ac51f373
Mateus Borges <borgesm.work@gmail.com>
2022-12-10 17:56:04 -0300

    feat: get rentals

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -1,5 +1,28 @@
+import { connection } from '../db/db.js';
+
 export async function getRentals(req, res) {
-	res.sendStatus(501);
+	const customerId = Number(req.query.customerId);
+	const gameId = Number(req.query.gameId);
+
+	try {
+		let rentals;
+		if (customerId && gameId) {
+			rentals = await connection.query('SELECT * FROM rentals WHERE "customerId"=$1 AND "gameId"=$2;', [
+				customerId,
+				gameId,
+			]);
+		} else if (customerId) {
+			rentals = await connection.query('SELECT * FROM rentals WHERE "customerId"=$1;', [customerId]);
+		} else if (gameId) {
+			rentals = await connection.query('SELECT * FROM rentals WHERE "gameId"=$1;', [gameId]);
+		} else {
+			rentals = await connection.query('SELECT * FROM rentals;');
+		}
+		res.send(rentals.rows);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 }
 
 export async function newRental(req, res) {
